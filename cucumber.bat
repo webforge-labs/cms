54613f2eeedca5e439fabf9faf95750816810538
Philipp Scheit <p.scheit@ps-webforge.com>
2016-03-09 19:56:23 +0100

    make cucumber.bat more intelligent

--- cucumber.bat
+++ cucumber.bat
@@ -1,3 +1,7 @@
 @echo off
 
-node_modules\cuked-zombie\node_modules\.bin\cucumber-js.cmd features/%1.feature --format=pretty -r tests/js/cucumber/bootstrap.js --snippet-syntax=tests/js/cucumber/snippet-template.js %2 %3 %4 %5
+if [%1]==[] (
+  node_modules\.bin\cucumber-js.cmd features --format=pretty -r tests/js/cucumber/bootstrap.js --snippet-syntax=tests/js/cucumber/snippet-template.js %1 %2 %3 %4
+) else (
+  node_modules\.bin\cucumber-js.cmd features/%1.feature --format=pretty -r tests/js/cucumber/bootstrap.js --snippet-syntax=tests/js/cucumber/snippet-template.js %2 %3 %4 %5
+)
