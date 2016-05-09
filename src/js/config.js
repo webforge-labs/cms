57a55b9d12644b239df2a899a1cc047e9d70f011
Philipp Scheit <p.scheit@ps-webforge.com>
2016-05-09 14:22:46 +0200

    add dropins locally for requirejs build

--- src/js/config.js
+++ src/js/config.js
@@ -2,14 +2,13 @@ var require = {
   baseUrl: '/assets/js',
 
   paths: {
-    "cms/modules/dropbox-chooser": "https://www.dropbox.com/static/api/2/dropins"
+    "cms/modules/dropbox-chooser": "cms/lib/dropbox-dropins"
   },
 
   shim: {
     "cms/modules/dropbox-chooser": {
       deps: ['admin/config'],
       exports: "Dropbox",
-
       init: function (config) {
         window.Dropbox.appKey = config.Dropbox.appKey;
         return window.Dropbox;
