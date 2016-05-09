a7e31fcdbb329ba6bd06cb54279afe8279113d9e
Philipp Scheit <p.scheit@ps-webforge.com>
2016-05-09 12:43:48 +0200

    fix config to be valid

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,15 @@
 var require = {
-  baseUrl: '/assets/js'
+  baseUrl: '/assets/js',
 
   paths: {
-    "cms/modules/dropbox-chooser": ["https://www.dropbox.com/static/api/2/dropins"]
+    "cms/modules/dropbox-chooser": "https://www.dropbox.com/static/api/2/dropins"
   },
 
   shim: {
     "cms/modules/dropbox-chooser": {
       deps: ['admin/config'],
       exports: "Dropbox",
+
       init: function (config) {
         window.Dropbox.appKey = config.Dropbox.appKey;
         return window.Dropbox;
