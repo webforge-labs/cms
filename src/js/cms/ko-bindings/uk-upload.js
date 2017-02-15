b89940558e8009cdf25ced4fb64d9680e1d638dd
Philipp Scheit <p.scheit@ps-webforge.com>
2017-02-15 09:30:39 +0100

    use the new uk-upload binding for the multilpe file choose as well

--- src/js/cms/ko-bindings/uk-upload.js
+++ src/js/cms/ko-bindings/uk-upload.js
@@ -1,90 +1,94 @@
 define(['require', 'knockout', 'jquery', 'uikit-src/uikit-core'], function(require, ko, $, UIkit) {
 
+  var UploadComponent = function(UIkit, element, options) {
+    var bar, warnings, files;
+
+    $context = $(element);
+
+    var defaultOptions = {
+      clsDragover: 'upload-area-hover',
+      multiple: true,
+      concurrent: 2,
+      dataType: 'json'
+    }
+
+    var uploadOptions = $.extend({}, defaultOptions, options, {
+
+      beforeAll: function(Upload, uploadFiles) { 
+        bar = $(element).find('.uk-progress')[0];
+        warnings = [];
+        files = [];
+
+        Upload.params['path'] = ko.isObservable(options.path) ? ko.unwrap(options.path) : options.path.call(Upload);
+
+        bar.max = 0;
+        ko.utils.arrayForEach(uploadFiles, function(file) {
+          bar.max += file.size;
+        });
+        bar.removeAttribute('hidden');
+        bar.value = 0;
+      },
+
+      progress: function (e) {
+        bar.value += e.loaded;
+      },
+
+      complete: function(xhr) {
+        var body = xhr.responseJSON;
+
+        ko.utils.arrayForEach(body.warnings, function(warning) {
+          warnings.push(warning);
+        });
+
+        ko.utils.arrayForEach(body.files, function(file) {
+          files.push(file);
+        });
+      },
+
+      completeAll: function(onlyLastXhr) {
+        if (warnings.length) {
+          $.notify({
+            message: 
+              files.length == 1 ? 
+                "Ich habe die Datei hochgeladen, aber:\n"+warnings.join("<br>\n") :
+                "Ich habe die "+files.length+" Dateien hochgeladen, aber:\n"+warnings.join("<br>\n")
+          },{
+            type: 'warning'
+          });
+        } else {
+          $.notify({
+            message: 
+              files.length == 1 ? 
+                "Okay, ich habe 1 Datei hochgeladen." :
+                "Okay, ich habe "+files.length+" Dateien hochgeladen."
+          },{
+            type: 'success'
+          });
+        }
+
+        if (options.completed) {
+          options.completed.call(undefined, files, onlyLastXhr);
+        }
+
+        setTimeout(function () {
+          bar.setAttribute('hidden', 'hidden');
+        }, 1000);
+      }
+    });
+
+    UIkit.upload($context.get(0), uploadOptions);
+  };
+
   ko.bindingHandlers.ukUpload = {
     init: function(element, valueAccessor, allBindingsAccessor, data, context) {
       var options = ko.unwrap(valueAccessor());
 
-      $context = $(element);
-
       require(['uikit', 'uikit-src/components/upload'], function(UIkit) {
-        var bar, warnings, files;
-
-        var defaultOptions = {
-          clsDragover: 'upload-area-hover',
-          multiple: true,
-          concurrent: 2,
-          dataType: 'json'
-        }
-
-        var uploadOptions = $.extend({}, defaultOptions, options, {
-
-          beforeAll: function(Upload, uploadFiles) { 
-            bar = $context.find('.uk-progress')[0];
-            warnings = [];
-            files = [];
-
-            Upload.params['path'] = options.path.call(Upload);
-
-            bar.max = 0;
-            ko.utils.arrayForEach(uploadFiles, function(file) {
-              bar.max += file.size;
-            });
-            bar.removeAttribute('hidden');
-            bar.value = 0;
-          },
-
-          progress: function (e) {
-            bar.value += e.loaded;
-          },
-
-          complete: function(xhr) {
-            var body = xhr.responseJSON;
-
-            ko.utils.arrayForEach(body.warnings, function(warning) {
-              warnings.push(warning);
-            });
-
-            ko.utils.arrayForEach(body.files, function(file) {
-              files.push(file);
-            });
-          },
-
-          completeAll: function(onlyLastXhr) {
-            if (warnings.length) {
-              $.notify({
-                message: 
-                  files.length == 1 ? 
-                    "Ich habe die Datei hochgeladen, aber:\n"+warnings.join("<br>\n") :
-                    "Ich habe die "+files.length+" Dateien hochgeladen, aber:\n"+warnings.join("<br>\n")
-              },{
-                type: 'warning'
-              });
-            } else {
-              $.notify({
-                message: 
-                  files.length == 1 ? 
-                    "Okay, ich habe 1 Datei hochgeladen." :
-                    "Okay, ich habe "+files.length+" Dateien hochgeladen."
-              },{
-                type: 'success'
-              });
-            }
-
-            if (options.completed) {
-              options.completed.call(undefined, files, onlyLastXhr);
-            }
-
-            setTimeout(function () {
-              bar.setAttribute('hidden', 'hidden');
-            }, 1000);
-          }
-        });
-
-        UIkit.upload($context, uploadOptions);
+        new UploadComponent(UIkit, element, options);
       });
-    },
+    }/*,
     update: function(element, valueAccessor, allBindingsAccessor, data, context) {
     }
+    */
   };
-
 });
--- src/js/cms/ko-components/multiple-files-chooser.js
+++ src/js/cms/ko-components/multiple-files-chooser.js
@@ -37,88 +37,18 @@ define(['require', 'knockout', 'jquery', 'knockout-collection', 'knockout-dragdr
       return params.generateUploadPath.call(null);
     });
 
+    that.addUploadedFiles = function(files) {
+      ko.utils.arrayForEach(files, function(exportedFile) {
+        that.files.add(exportedFile);
+      });
+    }
+
     that.reorder = function(item, newIndex, type) {
       //var oldIndex = filesObservableArray.indexOf(item);
       //console.log('moved from ', oldIndex, 'to ', newIndex);
       filesObservableArray.remove(item);
       filesObservableArray.splice(newIndex, 0, item);
     };
-
-    // this could be solved much nicer, if bar and upload would be knockout-components itself (TODO)
-    $context = $(params.uploadAreaSelector);
-
-    require(['uikit', 'uikit-src/components/upload'], function(UIkit) {
-      var bar, warnings, files;
-
-      UIkit.upload($context, {
-        url: '/cms/media/upload',
-        multiple: true,
-        concurrent: 2,
-        dataType: 'json',
-        clsDragover: 'upload-area-hover',
-
-        beforeAll: function(Upload, uploadFiles) { 
-          bar = $context.find('.uk-progress')[0];
-          warnings = [];
-          files = [];
-
-          Upload.params['path'] = that.uploadPath();
-
-          bar.max = 0;
-          ko.utils.arrayForEach(uploadFiles, function(file) {
-            bar.max += file.size;
-          });
-          bar.removeAttribute('hidden');
-          bar.value = 0;
-        },
-
-        progress: function (e) {
-          bar.value += e.loaded;
-        },
-
-        complete: function(xhr) {
-          var body = xhr.responseJSON;
-
-          ko.utils.arrayForEach(body.warnings, function(warning) {
-            warnings.push(warning);
-          });
-
-          ko.utils.arrayForEach(body.files, function(file) {
-            files.push(file);
-          });
-        },
-
-        completeAll: function(onlyLastXhr) {
-          if (warnings.length) {
-            $.notify({
-              message: 
-                files.length == 1 ? 
-                  "Ich habe die Datei hochgeladen, aber:\n"+warnings.join("<br>\n") :
-                  "Ich habe die "+files.length+" Dateien hochgeladen, aber:\n"+warnings.join("<br>\n")
-            },{
-              type: 'warning'
-            });
-          } else {
-            $.notify({
-              message: 
-                files.length == 1 ? 
-                  "Okay, ich habe 1 Datei hochgeladen." :
-                  "Okay, ich habe "+files.length+" Dateien hochgeladen."
-            },{
-              type: 'success'
-            });
-          }
-
-          ko.utils.arrayForEach(files, function(exportedFile) {
-            that.files.add(exportedFile);
-          });
-
-          setTimeout(function () {
-            bar.setAttribute('hidden', 'hidden');
-          }, 1000);
-        }
-      });
-    });
   };
 
   return { viewModel: filesChooser, template: htmlString };
