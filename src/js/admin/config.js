c061b50c9bfad07138288813b6fa2befb9c12fd2
Philipp Scheit <p.scheit@ps-webforge.com>
2017-06-13 10:16:14 +0200

    really add it

---
+++ src/js/admin/config.js
@@ -0,0 +1,14 @@
+define(['json!etc/cms/blocktypes.json'], function(blocktypes) {
+
+  return {
+    Dropbox: {
+      appKey: 'y2w4mmangd4nzis'
+    },
+
+    contentManager: {
+      blockTypes: blocktypes
+    }
+
+  };
+
+});
