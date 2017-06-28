c4abd286d94ea57d9720a86391d2215a3a710522
Philipp Scheit <p.scheit@ps-webforge.com>
2017-06-28 14:54:03 +0200

    fix empty properties in a block

--- src/php/Webforge/CmsBundle/Content/CommonBlockExtender.php
+++ src/php/Webforge/CmsBundle/Content/CommonBlockExtender.php
@@ -17,11 +17,13 @@ class CommonBlockExtender implements BlockExtender {
       $blockType = $context->config->getBlockType($block);
 
       foreach ($blockType->getProperties() as $property) {
+        $value = isset($block->{$property->name}) ? $block->{$property->name} : NULL;
+
         if ($property->hasMarkdown) {
-          $block->{$property->name.'Html'} = $this->markdowner->transformMarkdown($block->{$property->name});
+          $block->{$property->name.'Html'} = $this->markdowner->transformMarkdown($value);
         }
         if ($property->hasText) {
-          $block->{$property->name.'Text'} = $this->markdowner->transformText($block->{$property->name});
+          $block->{$property->name.'Text'} = $this->markdowner->transformText($value);
         }
       }
     }
