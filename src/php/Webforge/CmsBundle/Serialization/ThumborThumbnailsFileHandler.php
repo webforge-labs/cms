516fac6c54e93261d04cb166866847081fc7fc4e
Philipp Scheit <p.scheit@ps-webforge.com>
2017-07-24 12:29:17 +0200

    workaround thumbor bug with dirty getimagsize hack

--- src/php/Webforge/CmsBundle/Serialization/ThumborThumbnailsFileHandler.php
+++ src/php/Webforge/CmsBundle/Serialization/ThumborThumbnailsFileHandler.php
@@ -13,10 +13,11 @@ class ThumborThumbnailsFileHandler implements MediaFileHandlerInterface {
   protected $transformer;
   private $enabled;
 
-  public function __construct($transformations, $transformer) {
+  public function __construct($transformations, $transformer, $filesystem) {
     $this->transformer = $transformer;
     $this->transformations = $transformations;
     $this->enabled = count($this->transformations) > 0;
+    $this->filesystem = $filesystem;
   }
 
   public function serializeToFile(FileInterface $mediaFile, MediaFileEntityInterface $entity, stdClass $file) {
@@ -29,19 +30,17 @@ class ThumborThumbnailsFileHandler implements MediaFileHandlerInterface {
       }
 
       foreach ($this->transformations as $name => $transformation) {
-        $path = $mediaFile->getKey().'/'.$mediaFile->getName();
-
         $meta = new stdClass;
         $builder = $this->transformer->transform($file->url, $name);
 
         if (array_key_exists('metadata_only', $transformation) && $transformation['metadata_only']) {
           $metadataUrl = (string) $builder->build();
-          $this->fetchAndMergeMetadata($metadataUrl, $entity, 'thumbor.'.$name, $meta, $file);
 
           // build the real url without metadata
           $builder->metadataOnly(false);
           $meta->url = (string) $builder->build();
 
+          $this->fetchAndMergeMetadata($metadataUrl, $entity, 'thumbor.'.$name, $meta, $file);
         } else {
           $meta->url = (string) $builder->build();
         }
@@ -57,21 +56,58 @@ class ThumborThumbnailsFileHandler implements MediaFileHandlerInterface {
     $metadata = $entity->getMediaMetadata($cacheKey);
 
     if (!$metadata) {
+
+      /* this does not work right now, because: https://github.com/thumbor/thumbor/issues/949 
       $context = stream_context_create([
         'http'=>[
           'method'=> 'GET',
           'header'=>"Accept: application/json\r\n"
         ]
       ]);
-
       $metadata = json_decode(file_get_contents($url, false, $context))->thumbor;
+      */
+      $metadata = new \stdClass;
+
+      $physicalFile = $this->filesystem->get($file->key);
+      $size = getimagesizefromstring($physicalFile->getContent());
+
+      if ($size) {
+        $metadata->source = (object) [
+          'width'=>$size[0],
+          'height'=>$size[1]
+        ];
+      }
+
+      $context = stream_context_create([
+        'http'=>[
+          'method'=> 'GET'
+        ]
+      ]);
+
+      $size = getimagesizefromstring(file_get_contents($thumbnailMeta->url, false, $context));
+
+      if (!$size) {
+        throw new \Exception('cannot getimagesizefromstring for url: '.$thumbnailMeta->url);
+      }
+
+      $metadata->target = (object) [
+        'width'=>$size[0],
+        'height'=>$size[1]
+      ];
+
       $entity->setMediaMetadata($cacheKey, $metadata);
     }
 
     $thumbnailMeta->width = $metadata->target->width;
     $thumbnailMeta->height = $metadata->target->height;
+    $thumbnailMeta->isPortrait = $isPortrait = ($metadata->target->height > $metadata->target->width);
+    $thumbnailMeta->isLandscape = $metadata->target->width > $metadata->target->height;
+    $thumbnailMeta->orientation = $isPortrait ? 'portrait' : 'landscape'; // square === landscape
 
     $file->width = $metadata->source->width;
     $file->height = $metadata->source->height;
+    $file->isPortrait = $isPortrait = ($metadata->source->height > $metadata->source->width);
+    $file->isLandscape = $metadata->source->width > $metadata->target->height;
+    $file->orientation = $isPortrait ? 'portrait' : 'landscape'; // square === landscape
   }
 }
