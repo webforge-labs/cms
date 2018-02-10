0b3f56ef1188cc7faefceb99db9554e5e6340863
Philipp Scheit <p.scheit@ps-webforge.com>
2018-02-10 10:23:04 +0100

    remove console.log

--- src/js/cms/ko-components/multiple-files-chooser.js
+++ src/js/cms/ko-components/multiple-files-chooser.js
@@ -1,4 +1,4 @@
-define(['require', 'knockout', 'jquery', 'knockout-collection', 'knockout-dragdrop', 'text!./multiple-files-chooser.html', 'cms/modules/dispatcher', 'cms/ko-bindings/rubaxa-sortable'], function(require, ko, $, KnockoutCollection, koDragdrop, htmlString, dispatcher) {
+ define(['require', 'knockout', 'jquery', 'knockout-collection', 'knockout-dragdrop', 'text!./multiple-files-chooser.html', 'cms/modules/dispatcher', 'cms/ko-bindings/rubaxa-sortable'], function(require, ko, $, KnockoutCollection, koDragdrop, htmlString, dispatcher) {
 
   var filesChooser = function(params) {
     var that = this;
@@ -46,7 +46,7 @@ define(['require', 'knockout', 'jquery', 'knockout-collection', 'knockout-dragdr
 
     that.reorder = function(item, newIndex) {
       var oldIndex = filesObservableArray.indexOf(item);
-      console.log('moved from ', oldIndex, 'to ', newIndex);
+      //console.log('moved from ', oldIndex, 'to ', newIndex);
       
       filesObservableArray.remove(item);
       filesObservableArray.splice(newIndex, 0, item);
